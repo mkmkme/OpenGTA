6f50f33d6c75f8ccf4d63fd5bab87cd8f4b58b05
Reverend Homer <mk.43.ecko@gmail.com>
2021-06-03 18:10:38 +0300

    util: move implementation of log to source file

--- util/log.cpp
+++ util/log.cpp
@@ -1,5 +1,11 @@
 #include "log.h"
+#include "config.h"
 #include <SDL_opengl.h>
+
+#define ANSI_COLOR_OFF "\033[0m"
+#define ANSI_COLOR_INFO "\033[32m"
+#define ANSI_COLOR_WARN "\033[33m"
+#define ANSI_COLOR_ERR  "\033[31m"
 
 namespace Util {
   unsigned int Log::level = 0;
@@ -25,5 +31,48 @@ namespace Util {
         return "GL_OUT_OF_MEMORY";
     }
     return "Unknown-Error";
+  }
+
+  std::ostream &Log::info(const char *f, int l)
+  {
+    if (level) return emptyStream;
+    std::cout
+#ifdef LOG_USE_ANSI_COLORS
+    << ANSI_COLOR_INFO
+#endif
+    << "Info (" << f << ":" << l << "): "
+#ifdef LOG_USE_ANSI_COLORS
+    << ANSI_COLOR_OFF
+#endif
+    ;
+    return std::cout;
+  }
+
+  std::ostream &Log::warn(const char *f, int l)
+  {
+    if (level > 1) return emptyStream;
+    std::cerr
+#ifdef LOG_USE_ANSI_COLORS
+    << ANSI_COLOR_WARN
+#endif
+    << "Warning (" << f << ":" << l << "): "
+#ifdef LOG_USE_ANSI_COLORS
+    << ANSI_COLOR_OFF
+#endif
+    ;
+    return std::cerr;
+  }
+
+  std::ostream &Log::error(const char* f, int l) {
+    std::cerr
+#ifdef LOG_USE_ANSI_COLORS
+    << ANSI_COLOR_ERR
+#endif
+    << "Error (" << f << ":" << l << "): "
+#ifdef LOG_USE_ANSI_COLORS
+    << ANSI_COLOR_OFF
+#endif
+    ;
+    return std::cerr;
   }
 }

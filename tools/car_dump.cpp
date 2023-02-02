db711253ac450eec2ee1a9d289c5fca0317d4519
mkmkme <github@mkmk.aleeas.com>
2023-02-02 21:40:09 +0200

    tools/car_dump: don't rely on main2
    It's an ugly hack anyways

--- tools/car_dump.cpp
+++ tools/car_dump.cpp
@@ -1,79 +1,59 @@
-#include <cassert>
-#include <iostream>
-#include <string>
 #include "car-info.h"
 #include "dataholder.h"
 
-extern int global_Done;
-std::string map_file("undefined_map_file");
-std::string style_file("undefined_style_file");
-std::string msg_file("ENGLISH.FXT");
+#include <iostream>
+#include <string>
 
-void on_exit() {
-  PHYSFS_deinit();
-}
-
-void parse_args(int argc, char* argv[]) {
-#ifdef DUMP_CARS_IN_MAP
-  if (argc != 3) {
-    std::cout << "USAGE: " << argv[0] << " map-file style-file" << std::endl;
-    exit(1);
-  }
-  map_file = argv[1];
-  style_file = argv[2];
-#else
-  if (argc != 2) {
-    std::cout << "USAGE: " << argv[0] << " style-file" << std::endl;
-    exit(1);
-  }
-  map_file = "";
-  style_file = argv[1];
-#endif
-
-}
-
-void print_car(OpenGTA::CarInfo & ci) {
-  std::string model = "car" + std::to_string(int(ci.model));
-#define PRINT(c)  << #c << ":" << ci.c << "|"
-#define PRINTC(c) << #c << ":" << int(ci.c) << "|"
-  std::cout  PRINT(width)  PRINT(height)  PRINT(depth)
-  PRINT(sprNum) PRINT(weightDescriptor) PRINT(maxSpeed) PRINT(minSpeed)
-  PRINT(acceleration) PRINT(braking) PRINT(grip) PRINT(handling)
-  // remaps
-  PRINTC(vtype) PRINTC(model) PRINTC(turning) PRINTC(damagable) <<
-  "model-name:" << OpenGTA::MainMsgLookup::Instance().get().getText(model) << "|"
-  PRINTC(cx)  PRINTC(cy)  PRINT(moment) 
-  PRINT(rbpMass) PRINT(g1_Thrust) PRINT(tyreAdhesionX) PRINT(tyreAdhesionY)
-  PRINT(handBrakeFriction) PRINT(footBrakeFriction) PRINT(frontBrakeBias)
-  PRINT(turnRatio) PRINT(driveWheelOffset) PRINT(steeringWheelOffset)
-  PRINT(backEndSlideValue) PRINT(handBrakeSlideValue)
-  PRINTC(convertible) PRINTC(engine) PRINTC(radio) PRINTC(horn) PRINTC(soundFunction)
-  PRINTC(fastChangeFlag) PRINT(numDoors);
+void print_car(OpenGTA::CarInfo &ci)
+{
+    std::string model = "car" + std::to_string(int(ci.model));
+#define PRINT(c)  #c << ":" << ci.c << "|"
+#define PRINTC(c) #c << ":" << int(ci.c) << "|"
+    std::cout << PRINT(width) << PRINT(height) << PRINT(depth) << PRINT(sprNum) << PRINT(weightDescriptor)
+              << PRINT(maxSpeed) << PRINT(minSpeed) << PRINT(acceleration) << PRINT(braking) << PRINT(grip)
+              << PRINT(handling)
+              // remaps
+              << PRINTC(vtype) << PRINTC(model) << PRINTC(turning) << PRINTC(damagable)
+              << "model-name:" << OpenGTA::MainMsgLookup::Instance().get().getText(model) << "|" << PRINTC(cx)
+              << PRINTC(cy) << PRINT(moment) << PRINT(rbpMass) << PRINT(g1_Thrust) << PRINT(tyreAdhesionX)
+              << PRINT(tyreAdhesionY) << PRINT(handBrakeFriction) << PRINT(footBrakeFriction) << PRINT(frontBrakeBias)
+              << PRINT(turnRatio) << PRINT(driveWheelOffset) << PRINT(steeringWheelOffset) << PRINT(backEndSlideValue)
+              << PRINT(handBrakeSlideValue) << PRINTC(convertible) << PRINTC(engine) << PRINTC(radio) << PRINTC(horn)
+              << PRINTC(soundFunction) << PRINTC(fastChangeFlag) << PRINT(numDoors);
 #undef PRINT
-#define PRINT(c) << #c << ":" << ci.door[i].c << "|"
-  for (int i = 0; i < ci.numDoors; i++) {
-    std::cout PRINT(rpx) PRINT(rpy) PRINT(object) PRINT(delta);
-  }
-  std::cout << "remap8:";
-  for (int i = 0 ; i < 12; i++) {
-    std::cout << int(ci.remap8[i]) << (i < 11 ? "," : "");
-  }
-  std::cout << std::endl;
+#define PRINT(c) #c << ":" << ci.door[i].c << "|"
+    for (int i = 0; i < ci.numDoors; i++) {
+        std::cout << PRINT(rpx) << PRINT(rpy) << PRINT(object) << PRINT(delta);
+    }
+    std::cout << "remap8:";
+    for (int i = 0; i < 12; i++) {
+        std::cout << int(ci.remap8[i]) << (i < 11 ? "," : "");
+    }
+    std::cout << std::endl;
+#undef PRINT
 }
 
-void run_init(const char*) {
-  PHYSFS_init("mapview");
-  PHYSFS_mount(PHYSFS_getBaseDir(), nullptr, 1);
-  PHYSFS_mount("gtadata.zip", nullptr, 1);
-}
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        std::cerr << "USAGE: " << argv[0] << " STYLE_FILE" << std::endl;
+        return 1;
+    }
 
-// dump all cars in style
-void run_main() {
-  OpenGTA::MainMsgLookup::Instance().load(msg_file);
-  OpenGTA::ActiveStyle::Instance().load(style_file);
-  OpenGTA::GraphicsBase & style = OpenGTA::ActiveStyle::Instance().get();
-  std::cout << "DUMP_OBJ_INFO BEGIN" << std::endl;
-  for (auto &cinfo : style.carInfos)
-    print_car(cinfo);
+    const std::string style_file = argv[1];
 
+    PHYSFS_init("mapview");
+    PHYSFS_mount(PHYSFS_getBaseDir(), nullptr, 1);
+    PHYSFS_mount("gtadata.zip", nullptr, 1);
+
+    OpenGTA::MainMsgLookup::Instance().load("ENGLISH.FXT");
+    OpenGTA::ActiveStyle::Instance().load(style_file);
+    OpenGTA::GraphicsBase &style = OpenGTA::ActiveStyle::Instance().get();
+    std::cout << "DUMP_OBJ_INFO BEGIN" << std::endl;
+    for (auto &cinfo : style.carInfos) {
+        print_car(cinfo);
+    }
+
+    PHYSFS_deinit();
+    return 0;
 }

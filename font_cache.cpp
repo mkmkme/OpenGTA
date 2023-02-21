4507f5d62f1c531f7b4583e84a3fdc7430aa845d
mkmkme <github@mkmk.aleeas.com>
2023-02-21 20:45:20 +0200

    font_cache: simplify and refactor a bit

--- font_cache.cpp
+++ font_cache.cpp
@@ -1,57 +1,41 @@
 /************************************************************************
-* Copyright (c) 2005-2007 tok@openlinux.org.uk                          *
-*                                                                       *
-* This software is provided as-is, without any express or implied       *
-* warranty. In no event will the authors be held liable for any         *
-* damages arising from the use of this software.                        *
-*                                                                       *
-* Permission is granted to anyone to use this software for any purpose, *
-* including commercial applications, and to alter it and redistribute   *
-* it freely, subject to the following restrictions:                     *
-*                                                                       *
-* 1. The origin of this software must not be misrepresented; you must   *
-* not claim that you wrote the original software. If you use this       *
-* software in a product, an acknowledgment in the product documentation *
-* would be appreciated but is not required.                             *
-*                                                                       *
-* 2. Altered source versions must be plainly marked as such, and must   *
-* not be misrepresented as being the original software.                 *
-*                                                                       *
-* 3. This notice may not be removed or altered from any source          *
-* distribution.                                                         *
-************************************************************************/
+ * Copyright (c) 2005-2007 tok@openlinux.org.uk                          *
+ *                                                                       *
+ * This software is provided as-is, without any express or implied       *
+ * warranty. In no event will the authors be held liable for any         *
+ * damages arising from the use of this software.                        *
+ *                                                                       *
+ * Permission is granted to anyone to use this software for any purpose, *
+ * including commercial applications, and to alter it and redistribute   *
+ * it freely, subject to the following restrictions:                     *
+ *                                                                       *
+ * 1. The origin of this software must not be misrepresented; you must   *
+ * not claim that you wrote the original software. If you use this       *
+ * software in a product, an acknowledgment in the product documentation *
+ * would be appreciated but is not required.                             *
+ *                                                                       *
+ * 2. Altered source versions must be plainly marked as such, and must   *
+ * not be misrepresented as being the original software.                 *
+ *                                                                       *
+ * 3. This notice may not be removed or altered from any source          *
+ * distribution.                                                         *
+ ************************************************************************/
 #include "font_cache.h"
 
 #include <cassert>
 
 namespace OpenGTA {
-  FontCache::~FontCache() {
-    for (FontMap::iterator i = loadedFonts.begin(); i != loadedFonts.end(); i++) {
-      delete i->second;
-    }
-    loadedFonts.clear();
-  }
 
-  OpenGL::DrawableFont & FontCache::getFont(const std::string & file,
-    const uint32_t scale) {
-    FontMap::iterator i = findFont(file, scale);
-    if (i == loadedFonts.end()) {
-      OpenGL::DrawableFont* fnt = createFont(file, scale);
-      assert(fnt);
-      loadedFonts.insert(std::make_pair(FontIdentifier(file, scale), fnt));
-      return *fnt;
+OpenGL::DrawableFont &FontCache::getFont(const std::string &file, uint32_t scale)
+{
+    auto i = loaded_fonts_.find({ file, scale });
+    if (i == loaded_fonts_.end()) {
+        auto fnt = std::make_unique<OpenGL::DrawableFont>();
+        fnt->setScale(scale);
+        fnt->loadFont(file);
+        loaded_fonts_.insert({ FontIdentifier { file, scale }, std::move(fnt) });
+        return *fnt;
     }
     return *i->second;
-  }
-
-  FontCache::FontMap::iterator FontCache::findFont(const std::string & file, const uint32_t & scale) {
-    return loadedFonts.find(FontIdentifier(file, scale));
-  }
-
-  OpenGL::DrawableFont* FontCache::createFont(const std::string & file, const uint32_t & scale) {
-    OpenGL::DrawableFont * fnt = new OpenGL::DrawableFont();
-    fnt->setScale(scale);
-    fnt->loadFont(file);
-    return fnt;
-  }
 }
+} // namespace OpenGTA
--- font_cache.h
+++ font_cache.h
@@ -1,71 +1,63 @@
 /************************************************************************
-* Copyright (c) 2005-2007 tok@openlinux.org.uk                          *
-*                                                                       *
-* This software is provided as-is, without any express or implied       *
-* warranty. In no event will the authors be held liable for any         *
-* damages arising from the use of this software.                        *
-*                                                                       *
-* Permission is granted to anyone to use this software for any purpose, *
-* including commercial applications, and to alter it and redistribute   *
-* it freely, subject to the following restrictions:                     *
-*                                                                       *
-* 1. The origin of this software must not be misrepresented; you must   *
-* not claim that you wrote the original software. If you use this       *
-* software in a product, an acknowledgment in the product documentation *
-* would be appreciated but is not required.                             *
-*                                                                       *
-* 2. Altered source versions must be plainly marked as such, and must   *
-* not be misrepresented as being the original software.                 *
-*                                                                       *
-* 3. This notice may not be removed or altered from any source          *
-* distribution.                                                         *
-************************************************************************/
-#ifndef FONT_CACHE_H
-#define FONT_CACHE_H
-#include <string>
-#include <map>
+ * Copyright (c) 2005-2007 tok@openlinux.org.uk                          *
+ *                                                                       *
+ * This software is provided as-is, without any express or implied       *
+ * warranty. In no event will the authors be held liable for any         *
+ * damages arising from the use of this software.                        *
+ *                                                                       *
+ * Permission is granted to anyone to use this software for any purpose, *
+ * including commercial applications, and to alter it and redistribute   *
+ * it freely, subject to the following restrictions:                     *
+ *                                                                       *
+ * 1. The origin of this software must not be misrepresented; you must   *
+ * not claim that you wrote the original software. If you use this       *
+ * software in a product, an acknowledgment in the product documentation *
+ * would be appreciated but is not required.                             *
+ *                                                                       *
+ * 2. Altered source versions must be plainly marked as such, and must   *
+ * not be misrepresented as being the original software.                 *
+ *                                                                       *
+ * 3. This notice may not be removed or altered from any source          *
+ * distribution.                                                         *
+ ************************************************************************/
+#pragma once
+
 #include "gl_font.h"
 
+#include <map>
+#include <memory>
+#include <string>
+
 namespace OpenGTA {
-  
-  class FontCache {
-    public:
-      FontCache(const FontCache& copy) = delete;
-      FontCache& operator=(const FontCache& copy) = delete;
 
-      OpenGL::DrawableFont & getFont(const std::string & file, const uint32_t scale);
+class FontCache {
+public:
+    FontCache(const FontCache &copy) = delete;
+    FontCache &operator=(const FontCache &copy) = delete;
 
-      static FontCache& Instance()
-      {
+    OpenGL::DrawableFont &getFont(const std::string &file, const uint32_t scale);
+
+    static FontCache &Instance()
+    {
         static FontCache instance;
         return instance;
-      }
+    }
 
-    private:
-      FontCache() {}
-      ~FontCache();
-      struct FontIdentifier {
-        FontIdentifier(const std::string & f, const uint32_t s) : filename(f), scale(s) {}
+private:
+    FontCache() {}
+    struct FontIdentifier {
         const std::string filename;
         const uint32_t scale;
-        bool operator == (const FontIdentifier & o) const {
-          if (scale == o.scale && filename == o.filename)
-            return true;
-          return false;
+        bool operator==(const FontIdentifier &o) const { return scale == o.scale && filename == o.filename; }
+        bool operator<(const FontIdentifier &o) const
+        {
+            if (scale < o.scale)
+                return true;
+            if (scale > o.scale)
+                return false;
+            return filename < o.filename;
         }
-        bool operator < (const FontIdentifier & o) const {
-          if (scale < o.scale)
-            return true;
-          if (scale > o.scale)
-            return false;
-          return filename < o.filename;
-        }
-      };
-      typedef std::map<FontIdentifier, OpenGL::DrawableFont*> FontMap;
-      FontMap loadedFonts;
-      FontMap::iterator findFont(const std::string & file, const uint32_t & scale);
-      OpenGL::DrawableFont* createFont(const std::string & file, const uint32_t & scale);
-  };
-}
-
-#endif
+    };
+    std::map<FontIdentifier, std::unique_ptr<OpenGL::DrawableFont>> loaded_fonts_;
+};
+} // namespace OpenGTA

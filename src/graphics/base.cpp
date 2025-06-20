47b20de3e2f5ae46e780a75678eb2697e81ba8eb
Mikhail Koviazin <github@mkmk.aleeas.com>
2025-06-20 22:28:20 +0200

    graphics/base: remove unused include

--- src/graphics/base.cpp
+++ src/graphics/base.cpp
@@ -20,7 +20,7 @@
  * 3. This notice may not be removed or altered from any source          *
  * distribution.                                                         *
  ************************************************************************/
-#include <SDL2/SDL_opengl.h>
+// #include <SDL2/SDL_opengl.h>
 
 #include <graphics/base.h>
 

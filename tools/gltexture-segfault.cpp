3ba116b5bf9ba87986232b4542a9f7222c2dce10
Mikhail Koviazin <github@mkmk.aleeas.com>
2026-02-07 11:04:47 +0100

    fix macOS build

--- tools/gltexture-segfault.cpp
+++ tools/gltexture-segfault.cpp
@@ -2,8 +2,15 @@
 #ifdef _WIN32
 #include <windows.h>
 #endif
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
 #include <GL/gl.h>
 #include <GL/glu.h>
+#endif
+
 #include <fmt/base.h>
 
 int main()
